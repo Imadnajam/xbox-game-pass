6a878184514edaa838a259dfaf38f67b5bf248cb
imadnajam <118115991+Imadnajam@users.noreply.github.com>
2024-01-10 11:20:08 +0100

    setup the home page

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
 
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/',[HomeController::class,'Homepage'])->name('home');
