2f735ffe17a67f703dbd6170c188bb8b4cacc9b9
imadnajam <118115991+Imadnajam@users.noreply.github.com>
2024-01-19 17:51:50 +0100

    add home header in components

---
+++ resources/views/components/frontend/hero.blade.php
@@ -0,0 +1,22 @@
+
+<section class="hero" id="hero">
+    <div class="container">
+
+        <p class="hero-subtitle">The Season 8</p>
+
+        <h1 class="h1 hero-title">Steam</h1>
+
+        <div class="btn-group">
+
+            <button class="btn btn-primary">
+                <span>Watch live</span>
+
+                <ion-icon name="play-circle"></ion-icon>
+            </button>
+
+            <button class="btn btn-link">Dream making</button>
+
+        </div>
+
+    </div>
+</section>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -16,28 +16,7 @@
             <!--
         - #HERO
       -->
-
-            <section class="hero" id="hero">
-                <div class="container">
-
-                    <p class="hero-subtitle">The Season 8</p>
-
-                    <h1 class="h1 hero-title">Steam</h1>
-
-                    <div class="btn-group">
-
-                        <button class="btn btn-primary">
-                            <span>Watch live</span>
-
-                            <ion-icon name="play-circle"></ion-icon>
-                        </button>
-
-                        <button class="btn btn-link">Dream making</button>
-
-                    </div>
-
-                </div>
-            </section>
+      @include('components.frontend.hero')
 
 
 
