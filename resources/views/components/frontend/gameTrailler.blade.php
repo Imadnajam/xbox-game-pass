65a4c2bd6370cca25cf8c36311f376d3a0ebe8d5
imadnajam <118115991+Imadnajam@users.noreply.github.com>
2024-01-20 16:16:05 +0100

    add game trailler 2

--- resources/views/components/frontend/gameTrailler.blade.php
+++ resources/views/components/frontend/gameTrailler.blade.php
@@ -1,23 +1,25 @@
-
 @if(count($movies['results']) > 0)
-@foreach($movies['results'] as $movie)
-<div id="dd" style="width: 100%">
-    <div class="epic-slider-view">
-      <div class="slider" style="width: 100%">
-        <div style="width: 100%">
-          <div>
-            <video id="{{ $movie['id'] }}" poster="{{ $movie['preview'] }}" controls style="width: 100%">
-              <source src="{{ $movie['data']['480'] }}" type="video/mp4">
-            </video>
-          </div>
+    @foreach($movies['results'] as $movie)
+        <div id="dd" style="width: 100% ; display: flex; text-align: center; align-items: center; justify-content: center;">
+            <div class="epic-slider-view">
+                <div class="slider" style="width: 100%">
+                    <div style="width: 100%">
+                        <div>
+                            <h1>{{$movie['name']}}</h1><br>
+                            @if(isset($movie['data']['480']) && !empty($movie['data']['480']))
+                                <video id="{{ $movie['id'] }}" poster="{{ $movie['preview'] }}" controls style="width: 80%">
+                                    <source src="{{ $movie['data']['480'] }}" type="video/mp4">
+                                </video>
+                            @else
+                                <p>No trailer available for this movie.</p>
+                            @endif
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  </div>
-  
-@endforeach
+        @break <!-- This will exit the loop after processing the first movie -->
+    @endforeach
 @else
-  <p>No movies found.</p>
+    <p>No movies found.</p>
 @endif
-
- 
--- resources/views/components/frontend/hero.blade.php
+++ resources/views/components/frontend/hero.blade.php
@@ -2,19 +2,19 @@
 <section class="hero" id="hero">
     <div class="container">
 
-        <p class="hero-subtitle">The Season 8</p>
+        <p class="hero-subtitle">Découvre ton prochain jeu favori</p>
 
         <h1 class="h1 hero-title">Steam</h1>
 
         <div class="btn-group">
 
             <button class="btn btn-primary">
-                <span>Watch live</span>
+                <span>SOUSCRIVEZ POUR 1 €</span>
 
                 <ion-icon name="play-circle"></ion-icon>
             </button>
 
-            <button class="btn btn-link">Dream making</button>
+            <button class="btn btn-link"> Jouez à des centaines de jeux grâce à un abonnement mensuel à petit prix</button>
 
         </div>
 
