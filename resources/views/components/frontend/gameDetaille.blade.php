55cd0685ed26a60958260c639aedc8cf124a5c45
imadnajam <118115991+Imadnajam@users.noreply.github.com>
2024-01-20 12:54:25 +0100

    add game trailler

--- resources/views/components/frontend/gameDetaille.blade.php
+++ resources/views/components/frontend/gameDetaille.blade.php
@@ -4,14 +4,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
     <link rel="stylesheet" href="{{ asset('game/css/style.css') }}">
-    <!-- Bootstrap core CSS -->
-    <link href="demo-template/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-
-    <!-- Moovie CSS Style -->
-    <link rel="stylesheet" type="text/css" href=" {{ asset('game/js/moovie.css') }}">
-
-    <!-- GFonts -->
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100&display=swap" rel="stylesheet">
 
@@ -83,11 +77,20 @@
                 @endforeach
             </div>
         @endif
+       
     </div>
     <br>
 
     <br>
-    @include('components.frontend.gameTrailler', ['movies' => $movies])
+
+    
+        @include('components.frontend.gameTrailler', ['movies' => $movies])
+  
+    
+    
+
+  
+    
 
 
 
--- resources/views/components/frontend/gameTrailler.blade.php
+++ resources/views/components/frontend/gameTrailler.blade.php
@@ -1,43 +1,23 @@
 
 @if(count($movies['results']) > 0)
 @foreach($movies['results'] as $movie)
-
-  <div class="container">
-    <div class="row">
-      <div class="col-lg-12 text-center">
-        <div style='max-width: 800px; position: relative; margin: 0 auto; margin-top: 0px; margin-top: 64px;'>
-          <video id="{{ $movie['id'] }}" poster="{{ $movie['preview'] }}" controls>
-            <source src="{{ $movie['data']['480'] }}" type="video/mp4">
-            <!-- You can add more tracks for different languages if needed -->
-            <track kind="captions" label="English" srclang="en" src="https://raw.githubusercontent.com/BMSVieira/moovie.js/main/demo-template/subtitles/en.vtt">
-            Your browser does not support the video tag.
-          </video>
+<div id="dd" style="width: 100%">
+    <div class="epic-slider-view">
+      <div class="slider" style="width: 100%">
+        <div style="width: 100%">
+          <div>
+            <video id="{{ $movie['id'] }}" poster="{{ $movie['preview'] }}" controls style="width: 100%">
+              <source src="{{ $movie['data']['480'] }}" type="video/mp4">
+            </video>
+          </div>
         </div>
       </div>
     </div>
   </div>
+  
 @endforeach
 @else
   <p>No movies found.</p>
 @endif
 
-  <!-- Moovie JS Core -->
-  <script src="{{ asset('game/js/moovie.js') }}"></script>
-  <script>
-    document.addEventListener("DOMContentLoaded", function() {
-            var demo1 = new Moovie({
-              selector: "#example",
-              dimensions: {
-                   width: "100%"
-              },
-              config: {
-                  storage: {
-                     captionOffset: false,
-                     playrateSpeed: false,
-                     captionSize: false
-                  }
-              }
-            });
-    });
-  </script>
-
+ 
